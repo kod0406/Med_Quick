1589518c24446c78f61a4eef2149f3893ed242de
WhiteMilk <82200298+WhiteMilk357@users.noreply.github.com>
2024-06-02 16:41:16 +0900

    XML KT 수정
    시간 이름 분리

--- app/src/main/java/com/example/quick_med/Calendar.kt
+++ app/src/main/java/com/example/quick_med/Calendar.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import android.os.Bundle
 import android.widget.CalendarView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Calendar
 
 class Calendar : AppCompatActivity() {
 
     private lateinit var calendarView: CalendarView
-    private lateinit var alarmTextView: TextView
+    private lateinit var alarmTextTime: TextView
+    private lateinit var alarmTextName: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +19,8 @@ class Calendar : AppCompatActivity() {
 
         // CalendarView 초기화
         calendarView = findViewById(R.id.Calendar)
-        alarmTextView = findViewById(R.id.Alarm_TimeName)
+        alarmTextTime = findViewById(R.id.Alarm_Time)
+        alarmTextName = findViewById(R.id.Alarm_Name)
 
         // 날짜 선택 리스너 설정
         calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
@@ -43,15 +44,18 @@ class Calendar : AppCompatActivity() {
             val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) // 일요일 = 1, 월요일 = 2, ..., 토요일 = 7
 
             // 선택된 날짜의 요일에 알람이 있는지 확인하고 텍스트 초기화
-            alarmTextView.text = ""
+            alarmTextName.text = ""
+            alarmTextTime.text = ""
             if (alarmName != null && hour != -1 && minute != -1 && repeatDays[dayOfWeek - 1]) {
-                val displayHour = if (hour > 12) hour - 12 else if (hour == 0) 12 else hour
+                //val displayHour = if (hour > 12) hour - 12 else if (hour == 0) 12 else hour
                 val displayMinute = String.format("%02d", minute)
-                val amPm = if (hour >= 12) "PM" else "AM"
-                val alarmInfo = "$displayHour:$displayMinute $amPm : $alarmName\n"
-                alarmTextView.text = alarmInfo
+                //val amPm = if (hour >= 12) "PM" else "AM"
+                //val alarmInfo = "$displayHour:$displayMinute $amPm : $alarmName\n"
+                alarmTextName.text = alarmName
+                alarmTextTime.text = "$hour:$displayMinute"
             } else {
-                alarmTextView.text = "선택된 날짜에 알람이 없습니다."
+                alarmTextName.text = "선택된 날짜에 알람이 없습니다."
+                alarmTextTime.text = " "
             }
         }
     }
