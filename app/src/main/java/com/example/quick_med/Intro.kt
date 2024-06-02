316f0bb452157184197b157231ac5a1d365ece99
kod0406 <kod04061@gmail.com>
2024-06-02 20:22:20 +0900

    Update Additional_function
    로고 누끼 + 앱 아이콘 설정 + 연산시 "대기중" 추가

--- app/src/main/java/com/example/quick_med/Intro.kt
+++ app/src/main/java/com/example/quick_med/Intro.kt
@@ -1,17 +1,9 @@
 package com.example.quick_med
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import android.os.Handler
 import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.os.Handler
 import android.os.Looper
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -21,14 +13,17 @@ class Intro : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        // Ensure the ImageView is being set properly
-        val introImageView = findViewById<ImageView>(R.id.introImageView)
+        val sharedPreferences: SharedPreferences = getSharedPreferences("QuickMedPrefs", MODE_PRIVATE)
+        val isTermsAccepted = sharedPreferences.getBoolean("isTermsAccepted", false)
 
-        // 3초 후에  액티비티로 전환
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, Intro2::class.java)
+            val intent = if (isTermsAccepted) {
+                Intent(this, MainActivity::class.java)
+            } else {
+                Intent(this, Intro2::class.java)
+            }
             startActivity(intent)
-            finish() // 인트로 액티비티를 종료하여 뒤로 가기 버튼을 눌렀을 때 다시 돌아오지 않게 함
-        }, 3000) // 3000밀리초 == 3초
+            finish()
+        }, 3000)
     }
 }
